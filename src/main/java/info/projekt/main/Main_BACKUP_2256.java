20d5b495ab6b7291304bdd57064690a0114e0b40
Marcin Rys <rysjol@gmail.com>
2016-12-03 12:11:02 +0100

    GUI pierwszy

---
+++ src/main/java/info/projekt/main/Main_BACKUP_2256.java
@@ -0,0 +1,17 @@
+package info.projekt.main;
+
+import java.util.List;
+
+import info.projekt.dao.CustomersQuery;
+import info.projekt.database.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		CustomersQuery.customersList();
+
+	}
+
+}
---
+++ src/main/java/info/projekt/main/Main_BASE_2256.java
@@ -0,0 +1,13 @@
+package info.projekt.main;
+import info.projekt.database.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		
+	}
+
+}
